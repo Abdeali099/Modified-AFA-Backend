3830f53b3580e4f5ef890b950385aeae3f9ca0cb
Abdeali <abdealichharchhoda786@gmail.com>
2023-05-27 20:10:11 +0530

    Signup validation done

--- src/main/java/com/afa/authorization/signup/FilterSignup.java
+++ src/main/java/com/afa/authorization/signup/FilterSignup.java
@@ -1,12 +1,36 @@
 package com.afa.authorization.signup;
 
+import com.afa.database.ConnectionToDatabase;
 import com.afa.validation.DataValidation;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class FilterSignup implements Filter {
+
+    /* Objects and variables */
+    Connection connection = null;
+    PreparedStatement psForEmailValidation = null;
+    ResultSet rsForEmailValidation = null;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+        /* Establishing connection prepare statement and : only once */
+        try {
+            connection = ConnectionToDatabase.getConnection();
+            String queryForMailValidation = "SELECT * FROM user_details WHERE email = ? ;";
+            psForEmailValidation = connection.prepareStatement(queryForMailValidation);
+        } catch (Exception e) {
+            System.out.println("Error at signup filter connection establishment : " + e);
+        }
+
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
@@ -15,7 +39,7 @@ public class FilterSignup implements Filter {
 
         try {
 
-        HttpServletResponse response=(HttpServletResponse)servletResponse;
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
 
             /* Taking details from request */
             String userName = servletRequest.getParameter("afa_username");
@@ -28,8 +52,8 @@ public class FilterSignup implements Filter {
                     "\n => Phone : " + userPhone +
                     "\n => Password : " + userPassword);
 
-            /* Doing validation */
-            boolean isDataValidate=DataValidation.validateSignup(userName,userEmail,userPhone,userPassword);
+            /* Doing Data validation */
+            boolean isDataValidate = DataValidation.validateSignup(userName, userEmail, userPhone, userPassword);
 
             if (!isDataValidate) {
                 response.setStatus(400);
@@ -37,11 +61,57 @@ public class FilterSignup implements Filter {
                 return;
             }
 
+            /* <-- Checking user is already exist or not : duplicate email case --> */
+            boolean isUserAlreadyExist = isUserExist(userEmail);
+
+            if (isUserAlreadyExist) {
+                response.setStatus(400);
+                response.getWriter().println(false);
+                return;
+            }
+
             /* validation successful then forward to servlet */
+
+            response.setStatus(200);
+            response.getWriter().println(true);
 
         } catch (Exception exception) {
             System.out.println("Error in filter of signup : " + exception);
         }
 
     }
+
+    private boolean isUserExist(String userEmail) throws SQLException {
+
+        try {
+
+            psForEmailValidation.setString(1, userEmail);
+            rsForEmailValidation = psForEmailValidation.executeQuery();
+
+        } catch (Exception exception) {
+            System.out.println("Error in checking , is user exist or not : " + exception);
+        }
+
+        return rsForEmailValidation.next();
+    }
+
+    @Override
+    public void destroy() {
+
+        try {
+
+            if (psForEmailValidation != null) {
+                psForEmailValidation.close();
+            }
+
+            if (rsForEmailValidation != null) {
+                rsForEmailValidation.close();
+            }
+
+        } catch (Exception exception) {
+            System.out.println("Error in closing statement and result set ");
+        }
+
+    }
+
 }
